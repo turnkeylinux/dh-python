7dc5e729338964761f956efe9c0cff0a99ed6461
Stefano Rivera <stefanor@debian.org>
2022-08-19 17:06:02 +0200

    Add test coverage for pybuild.testfiles

--- tests/tpb02/foo/test_foo.py
+++ tests/tpb02/foo/test_foo.py
@@ -4,3 +4,8 @@ from unittest import TestCase
 class RequiredTest(TestCase):
     def test_tests_are_executed(self):
         open('test-executed', 'w').close()
+
+    def test_testfiles_exist(self):
+        open('testfile1.txt').close()
+        open('testfile2.txt').close()
+        open('testdir/testfile3.txt').close()
