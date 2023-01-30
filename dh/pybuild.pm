417af7ae8a57aa0d7c65276984155e13ca8e8230
Stefano Rivera <stefanor@debian.org>
2023-01-30 11:21:32 -0400

    pybuild.pm: Export SETUPTOOLS_SCM_PRETEND_VERSION for packages using python3-hatch-vcs packages.

--- dh/pybuild.pm
+++ dh/pybuild.pm
@@ -127,7 +127,7 @@ sub pybuild_commands {
 		# Without this, setuptools-scm tries to detect current
 		# version from git tag, which fails for debian tags
 		# (debian/<version>) sometimes.
-		if ((grep /(pypy|python[0-9\.]*)-setuptools-scm/, @deps) && !$ENV{'SETUPTOOLS_SCM_PRETEND_VERSION'}) {
+		if ((grep /python3-(setuptools-scm|hatch-vcs)/, @deps) && !$ENV{'SETUPTOOLS_SCM_PRETEND_VERSION'}) {
 			my $changelog = Dpkg::Changelog::Debian->new(range => {"count" => 1});
 			$changelog->load("debian/changelog");
 			my $version = @{$changelog}[0]->get_version();
